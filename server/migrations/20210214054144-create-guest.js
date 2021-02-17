5b50b10907106bf2cd302c958aad95b3d6b64d82
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-02-17 17:15:46 +0530

    updated migration

--- server/migrations/20210214054144-create-guest.js
+++ server/migrations/20210214054144-create-guest.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      invitedUserEmail: {
-        type: Sequelize.STRING,
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
